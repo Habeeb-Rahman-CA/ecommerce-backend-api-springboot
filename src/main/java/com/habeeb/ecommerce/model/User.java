f8184c45d4b341455428a30368f122795fb6c98a
Habeeb-Rahman-CA <habeebrahmanca07@gmail.com>
2025-03-17 22:53:28 +0530

    added: Roles and join the column with user

--- src/main/java/com/habeeb/ecommerce/model/User.java
+++ src/main/java/com/habeeb/ecommerce/model/User.java
@@ -1,10 +1,18 @@
 package com.habeeb.ecommerce.model;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 import org.hibernate.annotations.NaturalId;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,5 +29,9 @@ public class User {
     @NaturalId
     private String email;
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Collection<Role> roles = new HashSet<>();
 
 }
